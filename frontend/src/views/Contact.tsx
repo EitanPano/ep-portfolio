1f5b1c0e715777bd791d6bb79dce4ab57efdd9da
Eitan Pano <eitanpano95@gmail.com>
2022-08-02 14:23:03 +0300

    changed light/dark theme styles

--- frontend/src/views/Contact.tsx
+++ frontend/src/views/Contact.tsx
@@ -4,7 +4,7 @@ export const Contact = () => {
   return (
     <div className="contact main-layout background text">
         <section className="grid-container">
-            <h1>Contact Me : )</h1>
+            <h1>Contact Me</h1>
         </section>
     </div>
 );
