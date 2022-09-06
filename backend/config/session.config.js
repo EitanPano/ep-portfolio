30053e3d0a6ac470f22fb6a7bfcd11a99d8eb9cd
Eitan Pano <eitanpano95@gmail.com>
2022-09-06 19:24:55 +0300

    try to fix production leakage error

--- backend/config/session.config.js
+++ backend/config/session.config.js
@@ -1,7 +1,9 @@
 import expressSession from 'express-session';
+import MongoStore from 'connect-mongo';
 
 const sessionConfig = expressSession({
     secret: 'I Love Coding',
+    store: MongoStore.create(),
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
