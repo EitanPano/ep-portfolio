3db99639406a2a88e6c59be992c0a4942a48c6a7
Eitan Pano <eitanpano95@gmail.com>
2022-09-06 22:29:24 +0300

    removed connect mongo

--- backend/config/session.config.js
+++ backend/config/session.config.js
@@ -1,9 +1,7 @@
 import expressSession from 'express-session';
-import MongoStore from 'connect-mongo';
 
 const sessionConfig = expressSession({
     secret: 'I Love Coding',
-    store: MongoStore.create(),
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
